25bcd40d2246874d1ae30fbb153a3830fbb85972
Manish Sahu <manishnfriends@gmail.com>
2024-10-01 20:03:55 +0545

    first try

--- 404.php
+++ 404.php
@@ -1,26 +1,36 @@
+<!-- Header Start -->
 <?php
-/**
- * Template Name: Not found
- * Description: Page template 404 Not found.
- *
- */
-
 get_header();
-
-$search_enabled = get_theme_mod( 'search_enabled', '1' ); // Get custom meta-value.
+get_template_part( 'topbar');
 ?>
-<div id="post-0" class="content error404 not-found">
-	<h1 class="entry-title"><?php esc_html_e( 'Not found', 'ryc' ); ?></h1>
-	<div class="entry-content">
-		<p><?php esc_html_e( 'It looks like nothing was found at this location.', 'ryc' ); ?></p>
-		<div>
-			<?php
-				if ( '1' === $search_enabled ) :
-					get_search_form();
-				endif;
-			?>
-		</div>
-	</div><!-- /.entry-content -->
-</div><!-- /#post-0 -->
+		<div class="container-fluid bg-breadcrumb">
+            <div class="container text-center py-5" style="max-width: 900px;">
+                <h4 class="text-white display-4 mb-4 wow fadeInDown" data-wow-delay="0.1s">404 Pages</h4>
+                <ol class="breadcrumb d-flex justify-content-center mb-0 wow fadeInDown" data-wow-delay="0.3s">
+                    <li class="breadcrumb-item"><a href="index.html">Home</a></li>
+                    <li class="breadcrumb-item"><a href="#">Pages</a></li>
+                    <li class="breadcrumb-item active text-primary">404 Page</li>
+                </ol>    
+            </div>
+        </div>
+        <!-- Header End -->
+
+
+        <!-- 404 Start -->
+        <div class="container-fluid bg-light py-5">
+            <div class="container py-5 text-center">
+                <div class="row justify-content-center">
+                    <div class="col-lg-6 wow fadeInUp" data-wow-delay="0.3s">
+                        <i class="far fa-frown-open display-1 text-primary mb-4" style="width: 80px; height: 80px;"></i>
+                        <h1 class="display-1">404</h1>
+                        <h1 class="mb-4">Page Not Found</h1>
+                        <p class="mb-4">We’re sorry, the page you have looked for does not exist in our website! Maybe go to our home page or try to use a search?</p>
+                        <a class="btn btn-primary rounded-pill py-3 px-5" href="index.html">Go Back To Home</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- 404 End -->
 <?php
 get_footer();
+?>
--- header.php
+++ header.php
@@ -1,83 +1,41 @@
 <!DOCTYPE html>
-<html <?php language_attributes(); ?>>
-<head>
-	<meta charset="<?php bloginfo( 'charset' ); ?>">
-	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+<html lang="en">
 
-	<?php wp_head(); ?>
-</head>
+    <head>
+        <meta charset="utf-8">
+        <title>LifeSure - Life Insurance Website Template</title>
+        <meta content="width=device-width, initial-scale=1.0" name="viewport">
+        <meta content="" name="keywords">
+        <meta content="" name="description">
 
-<?php
-	$navbar_scheme   = get_theme_mod( 'navbar_scheme', 'navbar-light bg-light' ); // Get custom meta-value.
-	$navbar_position = get_theme_mod( 'navbar_position', 'static' ); // Get custom meta-value.
+        <!-- Google Web Fonts -->
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=DM+Sans:ital,opsz,wght@0,9..40,100..1000;1,9..40,100..1000&family=Inter:slnt,wght@-10..0,100..900&display=swap" rel="stylesheet">
 
-	$search_enabled  = get_theme_mod( 'search_enabled', '1' ); // Get custom meta-value.
-?>
+        <!-- Icon Font Stylesheet -->
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.4/css/all.css"/>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.1/font/bootstrap-icons.css" rel="stylesheet">
 
-<body <?php body_class(); ?>>
+        <!-- Libraries Stylesheet -->
+        <link rel="stylesheet" href="<?php echo get_template_directory_uri();?>/raw/lib/animate/animate.min.css"/>
+        <link href="<?php echo get_template_directory_uri();?>/raw/lib/lightbox/css/lightbox.min.css" rel="stylesheet">
+        <link href="<?php echo get_template_directory_uri();?>/raw/lib/owlcarousel/assets/owl.carousel.min.css" rel="stylesheet">
 
-<?php wp_body_open(); ?>
 
-<a href="#main" class="visually-hidden-focusable"><?php esc_html_e( 'Skip to main content', 'ryc' ); ?></a>
+        <!-- Customized Bootstrap Stylesheet -->
+        <link href="<?php echo get_template_directory_uri();?>/raw/css/bootstrap.min.css" rel="stylesheet">
 
-<div id="wrapper">
-	<header>
-		<nav id="header" class="navbar navbar-expand-md <?php echo esc_attr( $navbar_scheme ); if ( isset( $navbar_position ) && 'fixed_top' === $navbar_position ) : echo ' fixed-top'; elseif ( isset( $navbar_position ) && 'fixed_bottom' === $navbar_position ) : echo ' fixed-bottom'; endif; if ( is_home() || is_front_page() ) : echo ' home'; endif; ?>">
-			<div class="container">
-				<a class="navbar-brand" href="<?php echo esc_url( home_url() ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home">
-					<?php
-						$header_logo = get_theme_mod( 'header_logo' ); // Get custom meta-value.
+        <!-- Template Stylesheet -->
+        <link href="<?php echo get_template_directory_uri();?>/raw/css/style.css" rel="stylesheet">
+    </head>
 
-						if ( ! empty( $header_logo ) ) :
-					?>
-						<img src="<?php echo esc_url( $header_logo ); ?>" alt="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" />
-					<?php
-						else :
-							echo esc_attr( get_bloginfo( 'name', 'display' ) );
-						endif;
-					?>
-				</a>
+    <body>
 
-				<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbar" aria-controls="navbar" aria-expanded="false" aria-label="<?php esc_attr_e( 'Toggle navigation', 'ryc' ); ?>">
-					<span class="navbar-toggler-icon"></span>
-				</button>
-
-				<div id="navbar" class="collapse navbar-collapse">
-					<?php
-						// Loading WordPress Custom Menu (theme_location).
-						wp_nav_menu(
-							array(
-								'menu_class'     => 'navbar-nav me-auto',
-								'container'      => '',
-								'fallback_cb'    => 'WP_Bootstrap_Navwalker::fallback',
-								'walker'         => new WP_Bootstrap_Navwalker(),
-								'theme_location' => 'main-menu',
-							)
-						);
-
-						if ( '1' === $search_enabled ) :
-					?>
-							<form class="search-form my-2 my-lg-0" role="search" method="get" action="<?php echo esc_url( home_url( '/' ) ); ?>">
-								<div class="input-group">
-									<input type="text" name="s" class="form-control" placeholder="<?php esc_attr_e( 'Search', 'ryc' ); ?>" title="<?php esc_attr_e( 'Search', 'ryc' ); ?>" />
-									<button type="submit" name="submit" class="btn btn-outline-secondary"><?php esc_html_e( 'Search', 'ryc' ); ?></button>
-								</div>
-							</form>
-					<?php
-						endif;
-					?>
-				</div><!-- /.navbar-collapse -->
-			</div><!-- /.container -->
-		</nav><!-- /#header -->
-	</header>
-
-	<main id="main" class="container"<?php if ( isset( $navbar_position ) && 'fixed_top' === $navbar_position ) : echo ' style="padding-top: 100px;"'; elseif ( isset( $navbar_position ) && 'fixed_bottom' === $navbar_position ) : echo ' style="padding-bottom: 100px;"'; endif; ?>>
-		<?php
-			// If Single or Archive (Category, Tag, Author or a Date based page).
-			if ( is_single() || is_archive() ) :
-		?>
-			<div class="row">
-				<div class="col-md-8 col-sm-12">
-		<?php
-			endif;
-		?>
+        <!-- Spinner Start -->
+        <div id="spinner" class="show bg-white position-fixed translate-middle w-100 vh-100 top-50 start-50 d-flex align-items-center justify-content-center">
+            <div class="spinner-border text-primary" style="width: 3rem; height: 3rem;" role="status">
+                <span class="sr-only">Loading...</span>
+            </div>
+        </div>
+        <!-- Spinner End -->
